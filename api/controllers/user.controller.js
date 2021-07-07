89b3621a83c6a3b0278514805b47620e068916e4
SS1L <vlad7art13@gmail.com>
2021-07-08 00:18:58 +0300

    redesign restaurants and users controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,27 +1,26 @@
-const db = require('../db/database');
+const Users = require('../models/users.model');
 
 const getUsers = async (req, res) => {
   try {
-    const allUsers = await db.query('SELECT user_id, name, surname, address, user_phone FROM users');
-    if (!allUsers.rowCount) throw new SyntaxError('Something went wrong');
+    const allUsers = await Users.findAll();
 
-    res.status(200).json(allUsers.rows);
+    res.status(200).json({ data: allUsers });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
 };
 
-// email, password
 const createUser = async (req, res) => {
   try {
     const { name, surname, address, userPhone } = req.body;
 
-    await db.query(`INSERT INTO users (name, surname, address, user_phone)
-                    VALUES ($1, $2, $3, $4)`, [name, surname, address, userPhone]);
+    const newUser = await Users.create({
+      name, surname, address, user_phone: userPhone
+    }, { fields: ['name', 'surname', 'address', 'user_phone'] });
 
-    res.status(200).json({ message: 'User created' });
+    res.status(200).json({ message: 'User created', data: newUser });
   } catch (e) {
-    res.json({ error: e });
+    res.status(500).json({ error: e.message });
   }
 };
 
@@ -29,24 +28,26 @@ const updateUser = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, surname, address, userPhone } = req.body;
+    const user = await Users.update({
+      name, surname, address, user_phone: userPhone 
+    }, { where: { id } });
+    if (user[0] === 0) throw new SyntaxError("Can't find this user");
 
-    await db.query('UPDATE users SET name=$1, surname=$2, address=$3, user_phone=$4 WHERE user_id=$5', [name, surname, address, userPhone, id]);
-
-    res.status(200).json('User updated');
+    res.status(200).json({ message: 'User updated' });
   } catch (e) {
-    res.json({ error: e });
+    res.status(500).json({ error: e.message });
   }
 };
 
 const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await db.query(`DELETE FROM users WHERE user_id=${id}`);
-    if (!user.rowCount) throw new SyntaxError('Can`t find a user');
+    const user = await Users.destroy({ where: { id } });
+    if (!user) throw new SyntaxError("Can't find this user");
 
     res.status(200).json({ message: 'User deleted' });
   } catch (e) {
-    res.json({ error: e.message });
+    res.status(500).json({ error: e.message });
   }
 };
 
