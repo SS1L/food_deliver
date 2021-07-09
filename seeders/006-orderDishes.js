d8e0bfb6b9a707a9b9388b6a4ad4385aa6906e68
SS1L <vlad7art13@gmail.com>
2021-07-09 23:01:22 +0300

    update seeders

--- seeders/006-orderDishes.js
+++ seeders/006-orderDishes.js
@@ -8,6 +8,9 @@ module.exports = {
       { order_id: 1, dish_id: 1 },
       { order_id: 1, dish_id: 2 },
       { order_id: 2, dish_id: 4 },
+      { order_id: 3, dish_id: 2 },
+      { order_id: 4, dish_id: 5 },
+      { order_id: 5, dish_id: 1 },
     ]);
   },
 
