32ff9126453aefa7c049692cc5bff0bf93dcff6a
SS1L <vlad7art13@gmail.com>
2021-07-09 12:55:32 +0300

    update order controller

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,8 +1,8 @@
-const db = require('../db/database');
 const Orders = require('../models/order.model');
 const User = require('../models/users.model');
 const Restaurant = require('../models/restaurant.model');
 const Courier = require('../models/couriers.model');
+const Dish = require('../models/dishes.model');
 const orderDish = require('../models/orderDishes.model');
 
 const getOrders = async (req, res) => {
@@ -15,7 +15,6 @@ const getOrders = async (req, res) => {
   }
 };
 
-// need fix
 const getOrderId = async (req, res) => {
   try {
     const { id } = req.params;
@@ -27,37 +26,20 @@ const getOrderId = async (req, res) => {
         right: true,
       },
     });
+    if (!order) throw new SyntaxError("Can't find this order");
 
-    res.json(order);
-    // const order = await db.query(`SELECT  orders.order_id, user_id, restaurant_id, courier_id, total_price,dish_id  FROM orders INNER JOIN order_dish ON orders.order_id = order_dish.order_id WHERE orders.order_id=${id}`);
-    // if (!order.rowCount) throw new SyntaxError('Someting went wrong');
-
-    // const groupOrders = order.rows;
-    // const newDishId = [];
-    // groupOrders.forEach((element) => {
-    //  newDishId.push(element.dish_id);
-    // });
-    // console.log(groupOrders);
-
-    // res.status(200).json({
-    //   orderId: groupOrders[0].order_id,
-    //   userId: groupOrders[0].user_id,
-    //   restaurantId: groupOrders[0].restaurant_id,
-    //   courierId: groupOrders[0].courier_id,
-    //   totalPrice: groupOrders[0].total_price,
-    //   dishId: newDishId,
-    // });
+    res.json({ data: order });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
 };
 
-// need fix;
-const getNullOrder = async (req, res) => {
+const getAvailableOrder = async (req, res) => {
   try {
     const order = await Orders.findAll({ where: { courier_id: null } });
+    if (!order) throw new SyntaxError("Can't find any order");
 
-    res.status(200).json(order);
+    res.status(200).json({ data: order });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
@@ -65,38 +47,43 @@ const getNullOrder = async (req, res) => {
 
 const createOrder = async (req, res) => {
   try {
-    const { userId, restaurantId, courierId, dishId, total_price } = req.body;
+    const { userId, restaurantId, dishId } = req.body;
+    let totalPrice = 0;
+
+    const dish = await Dish.findAll({ where: { id: dishId } });
+    dish.forEach((element) => {
+      if (restaurantId !== element.restaurant_id) throw new SyntaxError('Something went wrong');
+      totalPrice += +element.price;
+    });
 
     const user = await User.findByPk(userId);
     if (!user) throw new SyntaxError("Can't find this user");
 
-    const restaurant = await Restaurant.findByPk(restaurantId);
-    if (!restaurant) throw new SyntaxError("Can't find this restaurant");
+    const order = await Orders.create(
+      { user_id: userId, restaurant_id: restaurantId, total_price: totalPrice.toFixed(2) }
+    );
+    if (!order.dataValues) throw new SyntaxError('Someting went wrong');
+    // need fix
+    for (let i = 0; i < dishId.length; i++) {
+      await orderDish.create({ order_id: order.dataValues.id, dish_id: dishId[i]});
+    }
 
-    const order = await Orders.build(
-      { user_id: userId, restaurant_id: restaurantId, total_price }
+    res.status(200).json({ data: order });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+const confirmOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { courierId } = req.body;
+    await Orders.update(
+      { courier_id: courierId },
+      { where: { id } }
     );
 
-    await order.save();
-
-//     const data = dishes.rows;
-//     let totalPrice = 0;
-//     data.forEach((elemet) => {
-//       if (data[0].restaurant_id !== restaurantId) throw new SyntaxError('You can`t order in many restaurant');
-//       totalPrice += +elemet.price.replace('$', '');
-//     });
-
-//     const d = await db.query(`INSERT INTO orders (user_id, restaurant_id, courier_id, total_price)
-//                   VALUES ($1, $2, $3, $4) RETURNING order_id`, [userId, restaurantId, courierId, totalPrice]);
-//     if (!d.rowCount) throw new SyntaxError('Something went wrong');
-//     console.log(d.rows[0].order_id);
-//     for (i in dishId) {
-//       db.query('INSERT INTO order_dish(order_id, dish_id) values($1, $2)', [d.rows[0].order_id, dishId[i]], (err) => {
-//         if (err) throw new SyntaxError('Something went wrong');
-//       });
-// }
-
-    res.status(200).json(order);
+    res.status(200).json({ message: 'Order confimed' });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
@@ -117,6 +104,8 @@ const deleteOrder = async (req, res) => {
 module.exports = {
   getOrders,
   getOrderId,
+  getAvailableOrder,
+  confirmOrder,
   createOrder,
   deleteOrder,
 };
--- api/routes/orders.routes.js
+++ api/routes/orders.routes.js
@@ -3,7 +3,9 @@ const order = require('../controllers/order.controller');
 
 router.get('/order', order.getOrders);
 router.get('/order/:id', order.getOrderId);
+router.get('/availableOrder', order.getAvailableOrder);
 router.post('/order', order.createOrder);
+router.put('/order/:id', order.confirmOrder);
 router.delete('/order/:id', order.deleteOrder);
 
 module.exports = router;
