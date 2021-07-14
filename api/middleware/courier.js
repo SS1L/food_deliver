bfe7cf98d4ed89c81613121c671836bc760b54d9
SS1L <vlad7art13@gmail.com>
2021-07-14 15:07:18 +0300

    change name of route

--- api/middleware/courier.js
+++ api/middleware/courier.js
@@ -1,8 +1,8 @@
 const Order = require('../models/order.model');
 
 const checkOrderCourier = async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const { id } = req.params;
     const data = await Order.findByPk(id);
     if (!data) return res.status(500).json({ message: "Can't find this order" });
 
--- api/routes/orders.routes.js
+++ api/routes/orders.routes.js
@@ -7,7 +7,7 @@ router.get('/order/:id', order.getOrderId);
 router.get('/availableOrder', order.getAvailableOrder);
 router.post('/order', order.createOrder);
 router.put('/order/:id', order.confirmOrder);
-router.put('/confirmOrder/:id', checkOrderCourier, order.deliveredOrder);
+router.put('/deliverOrder/:id', checkOrderCourier, order.deliveredOrder);
 router.delete('/order/:id', order.deleteOrder);
 
 module.exports = router;
